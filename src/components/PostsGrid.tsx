295d89f4a290f785e206c39483378fb4dbef1278
lk1 <whitecandy777@naver.com>
2023-09-06 23:56:51 +0900

    Design: 포스트 게시글 디자인

--- src/components/PostsGrid.tsx
+++ src/components/PostsGrid.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export default function PostsGrid(props: Props) {
   return (
-    <ul>
+    <ul className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {props.posts?.map((post, _) => (
         <li key={post.path}>
           <PostCard post={post} />
