64b2f596c0bceb82fbdd933c8c957480ffcfa240
lk1 <whitecandy777@naver.com>
2023-09-07 00:32:02 +0900

    캐러셀 기능 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 // Components
 import FeaturedPosts from "@/components/FeaturedPosts";
+import CarouselPosts from "@/components/CarouselPosts";
 import Profile from "@/components/Profile";
 
 export default function HomePage() {
@@ -8,6 +9,8 @@ export default function HomePage() {
       <Profile />
       {/* @ts-expect-error Server Component */}
       <FeaturedPosts />
+      {/* @ts-expect-error Server Component */}
+      <CarouselPosts />
     </>
   );
 }
