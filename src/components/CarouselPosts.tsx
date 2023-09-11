ba01aedd2af377e77ad5e9366ba32b248488ed4e
[LK1] <[ssomcandy777@gmail.com]>
2023-09-11 11:51:26 +0900

    메인 홈 캐러셀, 포스트 데이터 패딩 수정

--- src/components/CarouselPosts.tsx
+++ src/components/CarouselPosts.tsx
@@ -8,7 +8,7 @@ import MultiCarousel from "./MultiCarousel";
 export default async function CarouselPosts() {
   const posts = await getNonFeaturedPosts();
   return (
-    <section className="my-4">
+    <section className="my-4 px-4">
       <h2 className="text-2xl font-bold my-2">You may Like</h2>
       <MultiCarousel>
         {posts.map((post) => (
